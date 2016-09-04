fe77eed4abcab2b2ca1c22c20fbca6bb5d900b8b
Geraint <luffgd@gmail.com>
2016-09-04 21:06:57 +0100

    Separate out other modules, add presets

--- build.js
+++ build.js
@@ -2,11 +2,5 @@ var fs = require('fs');
 var pp = require('jsfx-preprocessor');
 
 var source = fs.readFileSync(__dirname + '/pad-synth.txt', {encoding: 'utf-8'});
-var targetFile = process.argv[2] || 'pad-synth';
+var targetFile = process.argv[2] || 'pad-synth.jsfx';
 fs.writeFileSync(targetFile, pp(source));
-
-if (process.argv[3]) {
-	var source = fs.readFileSync(__dirname + '/ui-lib.jsfx-inc', {encoding: 'utf-8'});
-	var targetFile = process.argv[3];
-	fs.writeFileSync(targetFile, pp(source));
-}
